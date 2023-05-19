b33c82757db4680097ec5f34db53ff03209bad37
Niksome <66631304+Niksome@users.noreply.github.com>
2023-05-19 18:03:34 +0200

    displaying the posts on the home route

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ const contactContent =
 
 const app = express();
 
+let posts = [];
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -33,8 +35,16 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
+  let title = req.body.postTitle;
   let content = req.body.postContent;
-  console.log(content);
+
+  let post = {
+    title: title,
+    content: content,
+  };
+
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
